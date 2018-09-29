c36abdc8fc1227fadcb260727b7d595227df5c1e
aakash <s533714@nwmissouri.edu>
2018-09-28 21:46:30 -0500

    COMMIT 1

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# aakash
+In thefirst file i have created my own web page
+In the second file i have done the calculation of the persons on houerly wages
+in the third file i have asked for the reviews for my page
---
+++ cal.js
@@ -0,0 +1,9 @@
+function wage(){
+
+let hour = document.getElementById("hour").value;
+let price = document.getElementById("price").value;
+
+alert("Your wage for the given hours and price is:"+hour*price);
+
+
+}
